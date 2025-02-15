4f21632f6436f29969d79428da6d42e1108835ca
Cristian Pineda <alternativec141@gmail.com>
2025-02-15 00:50:59 -0500

    changed a singular line

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const fs = require("fs");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;  // Use Railway's assigned port
 
 app.use(cors());
 app.use(express.json());
